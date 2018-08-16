4aa095090aef7de49952ea5fa0030c8a416a1569
Johan Martinsson <j.martinsson@bee-buzziness.com>
2018-08-16 23:02:24 +0100

    stopped where zip cant be applied to a list of arguments

--- haskell/src/lcd.hs
+++ haskell/src/lcd.hs
@@ -15,11 +15,11 @@ charToLcd char = Map.findWithDefault zeroLcd char numbers
                   where zeroLcd = ["", "", ""]
 
 toLcd :: String -> [String] 
-toLcd input = Prelude.map charToLcd . concatMap (++) $ input
+toLcd input = zipWith (++) $ Prelude.map charToLcd  input
 
 toLcdString :: String -> String
 toLcdString input = join "\n" $ toLcd input
 
 main :: IO ()
-main = print ( toLcdStringI "0123456789")
+main = print ( toLcdString "0123456789")
 
