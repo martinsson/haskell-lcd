f923bd5a312e6f171a3cc703c4bee64c8dac8d99
Johan Martinsson <j.martinsson@bee-buzziness.com>
2018-08-16 22:22:30 +0100

    now trying

--- haskell/src/lcd.hs
+++ haskell/src/lcd.hs
@@ -1,33 +1,25 @@
 -- import Test.Hspec
 import Data.Map.Strict as Map
 
-numbers :: Map Char String
-numbers = fromList [(0, [" _ ", 
-                         "| |",
-                         "|_|"]),
-                    (1, ["  |",
-                         "  |",
-                         "  |"])
+numbers :: Map Char [String]
+numbers = fromList [('0', [" _ ", 
+                           "| |",
+                           "|_|"]),
+                    ('1', ["  |",
+                           "  |",
+                           "  |"])
                    ]
 
 charToLcd :: Char -> [String]
-charToLcd char = Map.lookup numbers
+charToLcd char = Map.findWithDefault zeroLcd char numbers
+                  where zeroLcd = ["", "", ""]
 
 toLcd :: String -> [String] 
-toLcd input = input >>
-                toChars
-                charToLcd
-                concatMap
+toLcd input = Prelude.map charToLcd . concatMap (++) $ input
 
 toLcdString :: String -> String
 toLcdString input = join "\n" $ toLcd input
 
 main :: IO ()
-main :: print  $ toLcdStringI "0123456789"
+main = print ( toLcdStringI "0123456789")
 
--- main = hspec $ 
---   describe "toLcd" $ 
---     it "should transform single digit to lcd string" $ 
---       toLcd "0" `shouldBe` [" _ ", "| |"] 
---     it "should handle all digits" $
---       toLcd "0123456789" `shouldBe` [] 
