2e0e4a76b4e2c9a3a23c58d976cbdabb38ae6dcb
Johan Martinsson <j.martinsson@bee-buzziness.com>
2018-08-16 07:43:03 +0100

    start lcd

---
+++ haskell/src/lcd.hs
@@ -0,0 +1,10 @@
+-- import Test.Hspec
+-- import Data.List
+
+toLcd :: String -> [String] 
+toLcd input = [] 
+
+-- main = hspec $ 
+--   describe "toLcd" $ 
+--     it "should transform single digit to lcd string" $ 
+--       toLcd "0" `shouldBe` [" _ ", "| |"] 
