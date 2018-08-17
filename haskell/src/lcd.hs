c09b9a8a70fce2bb5e0fe82597e0a609799dd7c7
Johan Martinsson <j.martinsson@bee-buzziness.com>
2018-08-17 23:52:30 +0200

    got the mapMaybe working

--- haskell/src/lcd.hs
+++ haskell/src/lcd.hs
@@ -1,36 +1,47 @@
 -- import Test.Hspec
 import Data.List
 import Data.Map.Strict as Map
+import Data.Maybe as Maybe
 
-lcdDigits :: Map Char [String]
-lcdDigits = fromList [('0', [" _ ", 
-                           "| |",
-                           "|_|"]),
-                    ('1', ["   ",
+lcdDigits :: Map Char LcdString
+lcdDigits = fromList [('0', LcdString
+                             [" _ ", 
+                              "| |",
+                              "|_|"]),
+                    ('1', LcdString
+                          ["   ",
                            "  |",
                            "  |"]),
-                    ('2', [" _ ",
+                    ('2', LcdString
+                          [" _ ",
                            " _|",
                            "|_ "]),
-                    ('3', [" _ ",
+                    ('3', LcdString
+                          [" _ ",
                            " _|",
                            " _|"]),
-                    ('4', ["   ",
+                    ('4', LcdString
+                          ["   ",
                            "|_|",
                            "  |"]),
-                    ('5', [" _ ",
+                    ('5', LcdString
+                          [" _ ",
                            "|_ ",
                            " _|"]),
-                    ('6', [" _ ",
+                    ('6', LcdString
+                          [" _ ",
                            "|_ ",
                            "|_|"]),
-                    ('7', [" _ ",
+                    ('7', LcdString
+                          [" _ ",
                            "  |",
                            "  |"]),
-                    ('8', [" _ ",
+                    ('8', LcdString
+                          [" _ ",
                            "|_|",
                            "|_|"]),
-                    ('9', [" _ ",
+                    ('9', LcdString
+                          [" _ ",
                            "|_|",
                            " _|"])
                    ]
@@ -44,12 +55,11 @@ instance Monoid LcdString where
 instance Show LcdString where
   show (LcdString lines) = intercalate "\n" lines
 
-charToLcd :: Char -> LcdString 
-charToLcd char = LcdString $ Map.findWithDefault emptyLines char lcdDigits
-                  where emptyLines = ["", "", ""]
-
 toLcd :: String -> LcdString
-toLcd input = mconcat $ Prelude.map charToLcd  input
+toLcd input = mconcat $ Maybe.mapMaybe getLcdDigit input where
+                getLcdDigit :: Char -> Maybe LcdString
+                getLcdDigit char = Map.lookup char lcdDigits
+
 
 toLcdString :: String -> String
 toLcdString = show . toLcd
