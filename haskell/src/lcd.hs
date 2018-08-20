3120b7c8ee2b4512a0094ecc4d695db801b81493
Johan Martinsson <j.martinsson@bee-buzziness.com>
2018-08-20 22:41:45 +0200

    reorganize code

--- haskell/src/lcd.hs
+++ haskell/src/lcd.hs
@@ -5,6 +5,22 @@ import Data.List
 import Data.Map.Strict as Map
 import Data.Maybe as Maybe
 
+toLcdString :: String -> String
+toLcdString = show . mconcat . toLcdDigits
+
+newtype LcdString = LcdString [String]
+
+instance Monoid LcdString where
+  mempty = LcdString ["", "", ""]
+  mappend (LcdString linesFst) (LcdString linesSnd) = LcdString $ zipWith (++) linesFst linesSnd
+
+instance Show LcdString where
+  show (LcdString lines) = intercalate "\n" lines
+
+toLcdDigits :: String -> [LcdString]
+toLcdDigits input = Maybe.mapMaybe getLcdDigit input where
+                getLcdDigit char = Map.lookup char lcdDigits
+
 lcdDigits :: Map Char LcdString
 lcdDigits = fromList [('0', LcdString
                             [" _ ", 
@@ -47,20 +63,3 @@ lcdDigits = fromList [('0', LcdString
                              "|_|",
                              " _|"])]
                      
-
-newtype LcdString = LcdString [String]
-
-instance Monoid LcdString where
-  mempty = LcdString ["", "", ""]
-  mappend (LcdString linesFst) (LcdString linesSnd) = LcdString $ zipWith (++) linesFst linesSnd
-
-instance Show LcdString where
-  show (LcdString lines) = intercalate "\n" lines
-
-toLcdDigits :: String -> [LcdString]
-toLcdDigits input = Maybe.mapMaybe getLcdDigit input where
-                getLcdDigit char = Map.lookup char lcdDigits
-
-toLcdString :: String -> String
-toLcdString = show . mconcat . toLcdDigits
-
